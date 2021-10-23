b68bc89f8489c29a067628ea2fb97653b0409b92
Niklas <niklas.linz@enigmar.de>
2021-10-23 23:22:05 +0200

    update

--- src/main/java/de/linzn/webapi/WebApiPlugin.java
+++ src/main/java/de/linzn/webapi/WebApiPlugin.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2021. Niklas Linz - All Rights Reserved
- * You may use, distribute and modify this code under the
- * terms of the LGPLv3 license, which unfortunately won't be
- * written for another century.
+ *  Copyright (C) 2021. Niklas Linz - All Rights Reserved
+ *  You may use, distribute and modify this code under the
+ *  terms of the LGPLv3 license, which unfortunately won't be
+ *  written for another century.
  *
- * You should have received a copy of the LGPLv3 license with
- * this file. If not, please write to: niklas.linz@enigmar.de
+ *  You should have received a copy of the LGPLv3 license with
+ *  this file. If not, please write to: niklas.linz@enigmar.de
  *
  */
 
